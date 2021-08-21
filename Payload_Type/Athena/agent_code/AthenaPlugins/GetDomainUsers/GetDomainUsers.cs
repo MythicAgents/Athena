75efb04e2488cfbba859c746b8a696bf98d7632e
checkymander <checkymander@protonmail.com>
2021-08-21 03:01:32 -0400

    added GetDomainUsers plugin base

---
+++ Payload_Type/Athena/agent_code/AthenaPlugins/GetDomainUsers/GetDomainUsers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using CoreSploit.Enumeration;
+
+namespace Athena
+{
+    public static class Plugin
+    {
+        //We can pass dictionaries to functions. I just need to figure out how I want to do it on the agent side.
+        public static string Execute(Dictionary<string,object> args)
+        {
+            //Domain.DomainSearcher ds = new Domain.DomainSearcher();
+            string output = "";
+
+            output += "Int Value: " + args["int"];
+            output += "\r\nString Value: " + args["string"];
+            output += "\r\nBool Value: " + args["bool"];
+
+            return output;
+        }
+    }
+}
