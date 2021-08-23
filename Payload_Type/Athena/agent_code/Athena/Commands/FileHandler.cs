659f148078c47984d32f101d84343ac1bc252b59
checkymander <checkymander@protonmail.com>
2021-08-22 21:08:21 -0400

    update builder.py

---
+++ Payload_Type/Athena/agent_code/Athena/Commands/FileHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Athena.Commands
+{
+    public class FileHandler
+    {
+        public static byte[] downloadFile(string path)
+        {
+
+
+
+            return new byte[1];
+        }
+    }
+}
