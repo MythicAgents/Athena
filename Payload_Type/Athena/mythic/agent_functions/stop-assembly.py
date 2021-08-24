cb3cb956cf48d1d4c1a25a600dc35cd7a010241b
checkymander <checkymander@protonmail.com>
2021-08-24 12:42:44 -0400

    updated execute-assembly logic

--- Payload_Type/Athena/mythic/agent_functions/stop-assembly.py
+++ Payload_Type/Athena/mythic/agent_functions/stop-assembly.py
@@ -1,42 +1,32 @@
-from mythic_payloadtype_container.MythicCommandBase import *  # import the basics
-import json  # import any other code you might need
-# import the code for interacting with Files on the Mythic server
+from mythic_payloadtype_container.MythicCommandBase import *
+import json
 from mythic_payloadtype_container.MythicRPC import *
 
-# create a class that extends TaskArguments class that will supply all the arguments needed for this command
+
 class StopAssemblyArguments(TaskArguments):
     def __init__(self, command_line):
         super().__init__(command_line)
-        # this is the part where you'd add in your additional tasking parameters
+        self.args = {}
 
-    # you must implement this function so that you can parse out user typed input into your paramters or load your parameters based on some JSON input
     async def parse_arguments(self):
         pass
 
 
-# this is information about the command itself
-class ExecuteAssemblyCommand(CommandBase):
+class StopAssemblyCommand(CommandBase):
     cmd = "stop-assembly"
     needs_admin = False
     help_cmd = "stop-assembly"
-    description = "Stop the currently running assembly"
+    description = "Tasks Athena stop the currently running execute-assembly"
     version = 1
-    is_exit = False
-    is_file_browse = False
-    is_process_list = False
-    is_download_file = True
-    is_remove_file = False
-    is_upload_file = False
-    author = ""
-    argument_class = StopAssemblyArguments
+    supported_ui_features = ["callback_table:exit"]
+    author = "@checkymander"
     attackmapping = []
-    browser_script = None
-    browser_script = None
+    argument_class = StopAssemblyArguments
 
-    # this function is called after all of your arguments have been parsed and validated that each "required" parameter has a non-None value
     async def create_tasking(self, task: MythicTask) -> MythicTask:
         return task
 
     async def process_response(self, response: AgentResponse):
         pass
+
 
