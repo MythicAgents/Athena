326edeb7a493927bbb9e5f58ca53612f31ecc091
tr41nwr3ck <tr41nwr3ck>
2021-08-26 17:12:07 +0100

    env

---
+++ Payload_Type/Athena/agent_code/AthenaPlugins/env/env.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Athena
+{
+    public static class Plugin
+    {
+
+        public static string Execute(Dictionary<string, object> args)
+        {                                  
+            string Output = "";
+            foreach (DictionaryEntry fs in Environment.GetEnvironmentVariables())
+            {
+                Output += (fs.Key.ToString() + " = " + fs.Value.ToString() + Environment.NewLine);
+            }
+            return Output;
+
+        }
+    }
+}
