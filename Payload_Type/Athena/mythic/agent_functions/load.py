851e7ddf50e9285f3ed457e8d2b0543e2e295bfd
checkymander <checkymander@protonmail.com>
2021-08-27 12:30:16 -0400

    updated load.py for scripting

--- Payload_Type/Athena/mythic/agent_functions/load.py
+++ Payload_Type/Athena/mythic/agent_functions/load.py
@@ -31,7 +31,7 @@ class LoadCommand(CommandBase):
     argument_class = LoadArguments
 
     async def create_tasking(self, task: MythicTask) -> MythicTask:
-        dllFile = os.path.join(self.agent_code_path, "AthenaPlugins","bin", f"{task.args.command_line}.dll")
+        dllFile = os.path.join(self.agent_code_path, "AthenaPlugins","bin", f"{task.args.get_arg('name')}.dll")
         dllBytes = open(dllFile, 'rb').read()
         encodedBytes = base64.b64encode(dllBytes)
         task.args.add_arg("assembly", encodedBytes.decode())
