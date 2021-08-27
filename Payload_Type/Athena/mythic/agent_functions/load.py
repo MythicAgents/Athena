d9e04e4cf18669b5c657b947d73e1ea934aed99c
checkymander <checkymander@protonmail.com>
2021-08-27 12:23:39 -0400

    updated load.py for scripting

--- Payload_Type/Athena/mythic/agent_functions/load.py
+++ Payload_Type/Athena/mythic/agent_functions/load.py
@@ -12,7 +12,11 @@ class LoadArguments(TaskArguments):
     async def parse_arguments(self):
         if len(self.command_line) > 0:
             if self.command_line[0] == "{":
-                self.load_args_from_json_string(self.command_line)
+                j = json.loads(self.command_line)
+                self.add_arg("name", j["command"])
+            else:
+                self.add_arg("name", self.command_line)
+                # self.load_args_from_json_string(self.command_line)
 
 
 class LoadCommand(CommandBase):
@@ -30,8 +34,6 @@ class LoadCommand(CommandBase):
         dllFile = os.path.join(self.agent_code_path, "AthenaPlugins","bin", f"{task.args.command_line}.dll")
         dllBytes = open(dllFile, 'rb').read()
         encodedBytes = base64.b64encode(dllBytes)
-
-        task.args.add_arg("name", task.args.command_line)
         task.args.add_arg("assembly", encodedBytes.decode())
         return task
 
