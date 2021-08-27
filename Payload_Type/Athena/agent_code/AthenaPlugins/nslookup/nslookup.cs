b691a6008b1ea45f18514085195e45954fa707a1
checkymander <checkymander@protonmail.com>
2021-08-27 00:01:12 -0400

    add nslookup

---
+++ Payload_Type/Athena/agent_code/AthenaPlugins/nslookup/nslookup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+namespace Athena
+{
+    public static class Plugin
+    {
+        public static string Execute(Dictionary<string, object> args)
+        {
+            string output = "";
+            if (args.ContainsKey("hosts"))
+            {
+                string[] hosts = args["hosts"].ToString().Split(',');
+                foreach (var host in hosts)
+                {
+                    try
+                    {
+                        output += String.Format($"{host}\t\t{Dns.GetHostEntry(host)}") + Environment.NewLine;
+                    }
+                    catch (Exception e)
+                    {
+                        output += String.Format($"{host}\t\tNOTFOUND") + Environment.NewLine;
+                    }
+                }
+                return output;
+            }
+            else
+            {
+                return "No hosts specified!";
+            }
+        }
+    }
+}
