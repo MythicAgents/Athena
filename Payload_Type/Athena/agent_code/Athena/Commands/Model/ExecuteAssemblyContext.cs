02b87d19b1a51e0da56480bbde41eb97c1af9f78
checkymander <checkymander@protonmail.com>
2021-08-28 11:11:54 -0400

    code cleanup and fix for long running shell processes

---
+++ Payload_Type/Athena/agent_code/Athena/Commands/Model/ExecuteAssemblyContext.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.Loader;
+
+namespace Athena.Commands.Model
+{
+    public class ExecuteAssemblyContext : AssemblyLoadContext
+    {
+        public ExecuteAssemblyContext() : base(isCollectible: true)
+        {
+        }
+    }
+}
