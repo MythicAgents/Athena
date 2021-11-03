51a57c740110cc1f88e123f815dcaf786101c716
tr41nwr3ck <27775879+tr41nwr3ck@users.noreply.github.com>
2021-11-03 12:27:27 +0000

    added env Browser script

---
+++ Payload_Type/athena/mythic/browser_scripts/env.js
@@ -0,0 +1,45 @@
+function(task, responses){
+    if(task.status.includes("error")){
+        const combined = responses.reduce( (prev, cur) => {
+            return prev + cur;
+        }, "");
+        return {'plaintext': combined};
+    }else if(responses.length > 0){
+        let file = {};
+        let data = "";
+        let rows = [];
+        let headers = [
+            {"plaintext": "name", "type": "string", "cellStyle": {}, "width": 10},
+            {"plaintext": "value", "type": "string", "cellStyle": {}},
+        ];
+        for(let i = 0; i < responses.length; i++)
+        {
+            console.log(responses+[i])
+            try{
+                data = JSON.parse(responses[i]);
+            }catch(error){
+               const combined = responses.reduce( (prev, cur) => {
+                    return prev + cur;
+                }, "");
+                return {'plaintext': combined};
+            }
+            
+            for(let j = 0; j < data.length; j++){
+                let pinfo = data[j];
+                let row = {
+                    "rowStyle": {},
+                    "name": {"plaintext": pinfo["Name"], "cellStyle": {}},
+                    "value": {"plaintext": pinfo["Value"], "cellStyle": {}},
+                };
+                rows.push(row);
+            }
+        }
+        return {"table":[{
+            "headers": headers,
+            "rows": rows,
+            "title": "Environment Variables"
+        }]};
+    }else{
+        return {"plaintext": "Task Not Returned."};
+    }
+}
