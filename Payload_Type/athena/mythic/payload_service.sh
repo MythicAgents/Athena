6b668c9958d4b02be3b57964c40a48140e313286
checkymander <checkymander@protonmail.com>
2022-02-11 16:07:20 -0500

    fixed plugin issues

--- Payload_Type/athena/mythic/payload_service.sh
+++ Payload_Type/athena/mythic/payload_service.sh
@@ -5,7 +5,7 @@ cd /Mythic/agent_code/AthenaPlugins
 dotnet publish -c release
 
 cd /Mythic/mythic
-
+cp ~/.nuget/packages/system.directoryservices.protocols/6.0.0/runtimes/linux/lib/net6.0/System.DirectoryServices.Protocols.dll /Mythic/agent_code/AthenaPlugins/bin/windows/
 export PYTHONPATH=/Mythic:/Mythic/mythic
 
 python3.8 mythic_service.py
