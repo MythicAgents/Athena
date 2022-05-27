5b7777f104610ec9afd2075018f06edeceb4cc25
ascemama <antoine.scemama@yahoo.com>
2022-05-27 17:41:08 +0800

    beta

---
+++ Payload_Type/athena/mythic/agent_functions/powershell.py
@@ -0,0 +1,49 @@
+from mythic_payloadtype_container.MythicCommandBase import *
+import json
+
+
+class PowerShellArguments(TaskArguments):
+    def __init__(self, command_line, **kwargs):
+        super().__init__(command_line)
+        self.args = [
+            CommandParameter(
+                name="command",
+                type=ParameterType.String,
+                description="Command to be executed",
+                parameter_group_info=[ParameterGroupInfo(ui_position=1)],
+            ),
+        ]
+    async def parse_arguments(self):
+        if len(self.command_line.strip()) == 0:
+            raise Exception("powershell requires at least one command-line parameter.\n\tUsage: {}".format(PowerShellCommand.help_cmd))
+        else
+            self.args["path"].value = self.command_line
+         
+
+
+
+class PowerShellCommand(CommandBase):
+    cmd = "powershell"
+    needs_admin = False
+    help_cmd = "powershell [command] [arguments]"
+    description = "Run a powershell command in the agent process`"
+    version = 1
+    is_exit = False
+    is_file_browse = False
+    is_process_list = False
+    is_download_file = False
+    is_upload_file = False
+    is_remove_file = False
+    author = "@ascemama"
+    argument_class = PowerShellArguments
+    attackmapping = ["T1059", "T1059.004"]
+    attributes = CommandAttributes(
+        load_only=False,
+        builtin=True
+    )
+
+    async def create_tasking(self, task: MythicTask) -> MythicTask:
+        return task
+
+    async def process_response(self, response: AgentResponse):
+        pass
---
+++ documentation-payload/Athena/commands/powershell.md
@@ -0,0 +1,38 @@
++++
+title = "powershell"
+chapter = false
+weight = 10
+hidden = false
++++
+
+## Summary
+Execute a powershell command  
+  
+- Needs Admin: False  
+- Version: 1  
+- Author: @ascemama  
+
+### Arguments
+#### command
+
+- Description: The command to execute
+- Required Value: True  
+- Default Value: None  
+#### arguments
+
+- Description: The arguments to pass to the command 
+- Required Value: False  
+- Default Value: None  
+
+## Usage
+
+```
+powershell [command]
+```
+
+## MITRE ATT&CK Mapping
+
+- T1059  
+## Detailed Summary
+
+Execute a powershell command
