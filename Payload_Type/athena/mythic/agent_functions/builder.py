cb20853f222eb7601b1b37bd4e642135f6fd696d
checkymander <checkymander@protonmail.com>
2022-05-28 23:14:57 -0400

    dsf

--- Payload_Type/athena/mythic/agent_functions/builder.py
+++ Payload_Type/athena/mythic/agent_functions/builder.py
@@ -224,7 +224,7 @@ class athena(PayloadType):
                     f.write(baseConfigFile)
 
             if(self.get_parameter("native-aot") == True):
-                addLibrary(agent_build_path)
+                addNativeAot(agent_build_path)
 
             command = "nuget restore; dotnet publish -r {} -c {} --self-contained {} /p:PublishSingleFile={} /p:EnableCompressionInSingleFile={} /p:PublishReadyToRun={} /p:PublishTrimmed={}".format(self.get_parameter("rid"),self.get_parameter("configuration"), self.get_parameter("self-contained"), self.get_parameter("single-file"), self.get_parameter("compressed"),self.get_parameter("ready-to-run"), self.get_parameter("trimmed"))
             output_path = "{}/Athena/bin/Release/net6.0/{}/publish/".format(agent_build_path.name, self.get_parameter("rid"))
