d5beeaeaff49514e7ee8384c065bfb5e3bb9298b
checkymander <checkymander@protonmail.com>
2022-06-03 15:50:22 -0400

    a

--- Payload_Type/athena/mythic/agent_functions/ssh.py
+++ Payload_Type/athena/mythic/agent_functions/ssh.py
@@ -125,6 +125,7 @@ class SshArguments(TaskArguments):
                 elif temp_json["action"]=="exec":
                     self.set_arg("action", "exec")
                     self.set_arg("command", temp_json["command"])
+                    self.set_arg("command_line", self.command_line)
                 else:
                     self.load_args_from_json_string(self.command_line)
             else:
