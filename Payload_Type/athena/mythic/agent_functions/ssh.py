5925dbe8e4d989999ec99b1388e39204a5979ef2
checkymander <checkymander@protonmail.com>
2022-06-03 15:37:33 -0400

    proper parsing of array

--- Payload_Type/athena/mythic/agent_functions/ssh.py
+++ Payload_Type/athena/mythic/agent_functions/ssh.py
@@ -123,7 +123,7 @@ class SshArguments(TaskArguments):
                     self.set_arg("action" "switch")
                 elif temp_json["action"]=="exec":
                     self.set_arg("action", "exec")
-                    self.set_arg("command", "command")
+                    self.set_arg("command", temp_json["command"])
                 else:
                     self.load_args_from_json_string(self.command_line)
             else:
