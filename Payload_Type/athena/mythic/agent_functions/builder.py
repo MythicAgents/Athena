7a07ad0b448a7a0c5de881bea7e338385ef462f8
ascemama <antoine.scemama@yahoo.com>
2022-07-07 14:47:52 +0800

    set win10-x64 as first RID in the list instead of win-x64

--- Payload_Type/athena/mythic/agent_functions/builder.py
+++ Payload_Type/athena/mythic/agent_functions/builder.py
@@ -175,7 +175,7 @@ class athena(PayloadType):
         BuildParameter(
             name="rid",
             parameter_type=BuildParameterType.ChooseOne,
-            choices=["win-x64", "win-x86", "win-arm", "win-arm64", "win7-x64", "win7-x86", "win81-x64", "win81-arm", "win10-x64", "win10-x86", "win10-arm", "win10-arm64",
+            choices=["win10-x64", "win10-x86", "win-arm", "win-arm64", "win7-x64", "win7-x86", "win81-x64", "win81-arm", "win-x64", "win-x86", "win10-arm", "win10-arm64",
             "linux-x64", "linux-musl-x64","linux-arm","linux-arm64","rhel-x64","rhel.6-x64","tizen","tizen.4.0.0","tizen.5.0.0",
             "osx-x64","osx.10.10-x64","osx.10.11-x64","osx.10.12-x64","osx.10.13-x64","osx.10.14-x64","osx.10.15-x64","osx.11.0-x64","osx.11.0-arm64","osx.12-x64","osx.12-arm64"],
             default_value="win-x64",
