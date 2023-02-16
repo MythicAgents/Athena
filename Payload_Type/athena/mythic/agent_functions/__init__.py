b6a84d7ef4fc16e53810ad0983b6c40b35120ef9
checkymander <scottie.austin1991@gmail.com>
2023-02-16 00:06:19 -0500

    added outflank bofs to parse

--- Payload_Type/athena/mythic/agent_functions/__init__.py
+++ Payload_Type/athena/mythic/agent_functions/__init__.py
@@ -12,3 +12,13 @@ for x in modules:
         for el in dir(module):
             if "__" not in el:
                 globals()[el] = getattr(module, el)
+
+searchPath = currentPath.parent / "outflank_bofs" / "*.py"
+modules = glob.glob(f"{searchPath}")
+invalidate_caches()
+for x in modules:
+    if not x.endswith("__init__.py") and x[-3:] == ".py":
+        module = import_module(f"{__name__}.outflank_bofs." + Path(x).stem) 
+        for el in dir(module):
+            if "__" not in el:
+                globals()[el] = getattr(module, el)
