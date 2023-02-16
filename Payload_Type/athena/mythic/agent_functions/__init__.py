ebecd72bf998038ef43188d5d8784c4a3d5579af
checkymander <scottie.austin1991@gmail.com>
2023-02-15 23:10:39 -0500

    added adcs_enum bof for example trusted sec bof process

--- Payload_Type/athena/mythic/agent_functions/__init__.py
+++ Payload_Type/athena/mythic/agent_functions/__init__.py
@@ -3,12 +3,12 @@ from pathlib import Path
 import glob
 
 currentPath = Path(__file__)
-searchPath = currentPath.parent / "test_bofs" / "*.py"
+searchPath = currentPath.parent / "trusted_sec_bofs" / "*.py"
 modules = glob.glob(f"{searchPath}")
 invalidate_caches()
 for x in modules:
     if not x.endswith("__init__.py") and x[-3:] == ".py":
-        module = import_module(f"{__name__}.test_bofs." + Path(x).stem)
+        module = import_module(f"{__name__}.test_bofs." + Path(x).stem) 
         for el in dir(module):
             if "__" not in el:
                 globals()[el] = getattr(module, el)
