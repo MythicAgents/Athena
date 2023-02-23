56c3e4dcb6372290582850e9b2473953191eb963
tr41nwr3ck <27775879+tr41nwr3ck@users.noreply.github.com>
2023-02-23 17:56:23 +0000

    Update screenshot.py
    syntax

--- Payload_Type/athena/mythic/agent_functions/screenshot.py
+++ Payload_Type/athena/mythic/agent_functions/screenshot.py
@@ -35,10 +35,9 @@ class ScreenshotCommand(CommandBase):
         return task
 
     async def process_response(self, response: AgentResponse):
-        # file_resp = await MythicRPC().execute("create_file",
-        #                         task_id=task.id,
-        #                         file=user_output[1],
-        #                         delete_after_fetch=False,
-        #                         is_screenshot=True)    
-        # return task
-        pass
+            file_resp = await MythicRPC().execute("create_file",
+                                    task_id=task.id,
+                                    file=user_output[1],
+                                    delete_after_fetch=False,
+                                    is_screenshot=True)    
+            return task
