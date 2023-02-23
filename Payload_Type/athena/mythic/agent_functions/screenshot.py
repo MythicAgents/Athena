2ddafe837c2f8f3f39dd34e04c378cc3d3448e68
tr41nwr3ck <27775879+tr41nwr3ck@users.noreply.github.com>
2023-02-23 14:38:30 +0000

    Update screenshot.py
    Base Update for Screenshot post processing

--- Payload_Type/athena/mythic/agent_functions/screenshot.py
+++ Payload_Type/athena/mythic/agent_functions/screenshot.py
@@ -31,10 +31,37 @@ class ScreenshotCommand(CommandBase):
     async def create_tasking(self, task: MythicTask) -> MythicTask:
         return task
 
+    async def create_tasking(self, task: MythicTask) -> MythicTask:
+        task.completed_callback_function = self.screenshot_completed
+        return task
+
     async def process_response(self, response: AgentResponse):
             file_resp = await MythicRPC().execute("create_file",
                                     task_id=response.task.id,
                                     file=response.response,
                                     delete_after_fetch=False,
                                     is_screenshot=True)  
-pass
+    pass
+    async def screenshot_completed(self, task: MythicTask, subtask: dict = None, subtask_group_name: str = None) -> MythicTask:
+        if task.completed and task.status != MythicStatus.Error:
+            responses = await MythicRPC().execute(
+                "get_responses",
+                task_id=task.id,
+            )
+            if responses.status != MythicStatus.Success:
+                raise Exception("Failed to get responses from task")
+            file_id = ""
+            for f in responses.response["files"]:
+                if "agent_file_id" in f.keys() and f["agent_file_id"] != "" and f["agent_file_id"] != None:
+                    file_id = f["agent_file_id"]
+                    break
+            if file_id == "":
+                raise Exception("Screenshot completed successfully, but no files had an agent_file_id")
+            else:
+                resp = await MythicRPC().execute(
+                    "create_output",
+                    task_id=task.id,
+                    output=file_id)
+                if resp.status != MythicStatus.Success:
+                    raise Exception("Failed to create output")
+        return task
