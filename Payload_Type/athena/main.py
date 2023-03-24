66be18af08e36e00dcd536d59b9cb2804a9a61bf
checkymander <scottie.austin1991@gmail.com>
2023-03-24 16:23:50 -0400

    quited down the build process of plugins

--- Payload_Type/athena/main.py
+++ Payload_Type/athena/main.py
@@ -2,6 +2,6 @@ import mythic_container
 from athena.mythic import *
 import subprocess
 
-p = subprocess.Popen(["dotnet", "build"], cwd="/Mythic/athena/agent_code/AthenaPlugins")
+p = subprocess.Popen(["dotnet", "build", "--verbosity q", "--nologo"], cwd="/Mythic/athena/agent_code/AthenaPlugins")
 
 mythic_container.mythic_service.start_and_run_forever()
