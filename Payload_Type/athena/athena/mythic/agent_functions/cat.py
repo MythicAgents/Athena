490deef2e93a1e306164bd2e2e10fe2608d5bad2
checkymander <scottie.austin1991@gmail.com>
2023-03-24 19:12:23 -0400

    sdfsd

--- Payload_Type/athena/athena/mythic/agent_functions/cat.py
+++ Payload_Type/athena/athena/mythic/agent_functions/cat.py
@@ -1,7 +1,7 @@
 from mythic_container.MythicCommandBase import *
 import json
 from mythic_container.MythicRPC import *
-from .athena_messages import message_converter
+#from .athena_messages import message_converter
 
 
 class CatArguments(TaskArguments):
@@ -44,6 +44,6 @@ class CatCommand(CommandBase):
         return task
 
     async def process_response(self, task: PTTaskMessageAllData, response: any) -> PTTaskProcessResponseMessageResponse:
-        resp = PTTaskProcessResponseMessageResponse(TaskID=task.Task.ID, Success=True)
-        await MythicRPC().execute("create_output", task_id=task.Task.ID, output=translateAthenaMessage(resp.))
+        # resp = PTTaskProcessResponseMessageResponse(TaskID=task.Task.ID, Success=True)
+        # await MythicRPC().execute("create_output", task_id=task.Task.ID, output=translateAthenaMessage(resp.))
         return resp
