dd5c2bd4416b3c563711fc8c6011c24d7cfa2827
checkymander <scottie.austin1991@gmail.com>
2023-06-12 10:51:11 -0400

    gdf

--- Payload_Type/athena/athena/mythic/agent_functions/nslookup.py
+++ Payload_Type/athena/athena/mythic/agent_functions/nslookup.py
@@ -51,22 +51,20 @@ class NsLookupCommand(CommandBase):
 
 
     async def create_go_tasking(self, taskData: PTTaskMessageAllData) -> PTTaskCreateTaskingMessageResponse:
+        response = PTTaskCreateTaskingMessageResponse(
+            TaskID=taskData.Task.ID,
+            Success=True,
+        )
         groupName = taskData.args.get_parameter_group_name()
 
         if groupName == "TargetList":
-            fData = FileData()
-            fData.AgentFileId = taskData.args.get_arg("inputlist")
-            file = await SendMythicRPCFileGetContent(fData)
+            file = await SendMythicRPCFileGetContent(MythicRPCFileGetContentMessage(taskData.args.get_arg("inputlist")))
             
             if file.Success:
                 file_contents = base64.b64encode(file.Content)
                 taskData.args.add_arg("targetlist", file_contents.decode("utf-8"))
             else:
                 raise Exception("Failed to get file contents: " + file.Error)
-        response = PTTaskCreateTaskingMessageResponse(
-            TaskID=taskData.Task.ID,
-            Success=True,
-        )
         return response
 
     async def process_response(self, task: PTTaskMessageAllData, response: any) -> PTTaskProcessResponseMessageResponse:
