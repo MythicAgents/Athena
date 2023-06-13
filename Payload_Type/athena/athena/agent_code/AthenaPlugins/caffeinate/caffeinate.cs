df7e06fb6bbaafe8bb1afa8f2b71763480c5b658
checkymander <scottie.austin1991@gmail.com>
2023-06-13 16:00:51 -0400

    added caffeinate plugin

---
+++ Payload_Type/athena/athena/agent_code/AthenaPlugins/caffeinate/caffeinate.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using Athena.Commands;
+using Athena.Commands.Models;
+
+namespace Plugins
+{
+    public class Caffeinate : AthenaPlugin
+    {
+        [FlagsAttribute]
+        public enum EXECUTION_STATE : uint
+        {
+            ES_AWAYMODE_REQUIRED = 0x00000040,
+            ES_CONTINUOUS = 0x80000000,
+            ES_DISPLAY_REQUIRED = 0x00000002,
+            ES_SYSTEM_REQUIRED = 0x00000001
+            // Legacy flag, should not be used.
+            // ES_USER_PRESENT = 0x00000004
+        }
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
+        public override string Name => "caffeinate";
+        private static bool running = false;
+        public override void Execute(Dictionary<string, string> args)
+        {
+            try
+            {
+                if (running)
+                {
+                    TaskResponseHandler.Write("Letting computer sleep", args["task-id"], true);
+                    SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS);
+                }
+                else
+                {
+                    TaskResponseHandler.Write("Keeping PC awake", args["task-id"], true);
+                    SetThreadExecutionState(EXECUTION_STATE.ES_DISPLAY_REQUIRED | EXECUTION_STATE.ES_CONTINUOUS);
+                }
+
+            }
+            catch (Exception e)
+            {
+                TaskResponseHandler.Write(e.ToString(), args["task-id"], true, "error");
+            }
+        }
+    }
+}
+
---
+++ Payload_Type/athena/athena/mythic/agent_functions/caffeinate.py
@@ -0,0 +1,37 @@
+from mythic_container.MythicCommandBase import *
+import json
+from mythic_container.MythicRPC import *
+from .athena_utils import message_converter
+
+class CaffeinateArguments(TaskArguments):
+    def __init__(self, command_line, **kwargs):
+        super().__init__(command_line)
+        self.args = []
+
+    async def parse_arguments(self):
+        pass
+
+
+class CaffeinateCommand(CommandBase):
+    cmd = "cafeeinate"
+    needs_admin = False
+    help_cmd = "caffeinate"
+    description = "Keep the computer awake, or let the computer sleep if it's already caffeinated"
+    version = 1
+    author = "@checkymander"
+    argument_class = CaffeinateArguments
+    attackmapping = ["T1005", "T1552.001"]
+    #completion_functions: dict[str, Callable[[PTTaskCompletionFunctionMessage], Awaitable[PTTaskCompletionFunctionMessageResponse]]] = {}
+    #attackmapping = []
+    attributes = CommandAttributes(
+    )
+
+    async def create_go_tasking(self, taskData: PTTaskMessageAllData) -> PTTaskCreateTaskingMessageResponse:
+        response = PTTaskCreateTaskingMessageResponse(
+            TaskID=taskData.Task.ID,
+            Success=True,
+        )
+        return response
+
+    async def process_response(self, task: PTTaskMessageAllData, response: any) -> PTTaskProcessResponseMessageResponse:
+        pass
