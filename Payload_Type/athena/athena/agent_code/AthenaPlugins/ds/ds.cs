04a11a35de48745209b01ecc77d978875e8a1f9d
checkymander <scottie.austin1991@gmail.com>
2023-06-26 16:29:41 -0400

    test parsing for ldap

--- Payload_Type/athena/athena/agent_code/AthenaPlugins/ds/ds.cs
+++ Payload_Type/athena/athena/agent_code/AthenaPlugins/ds/ds.cs
@@ -199,21 +199,34 @@ namespace Plugins
 
 
                 //Dictionary<string, string> rez = new Dictionary<string, string>();
-                Dictionary<string, List<string>> attributes = new Dictionary<string, List<string>>();
+                Dictionary<string, string> attributes = new Dictionary<string, string>();
                 foreach (SearchResultEntry entry in response.Entries)
                 {
-                    attributes.Add(entry.DistinguishedName, new List<string>());
-                    foreach (var attr in entry.Attributes)
-                    {
-                        try
-                        {
-                            attributes[entry.DistinguishedName].Add(Misc.Base64Decode(attr.ToString()).Result);
-                        }
-                        catch
-                        {
-                            attributes[entry.DistinguishedName].Add(attr.ToString());
-                        }
-                    }
+                    attributes.Add(entry.DistinguishedName, Plugin.ldapconverter.ParseResult(entry));
+
+
+                    //foreach (DirectoryAttribute attr in entry.Attributes)
+                    //{
+                    //    Plugin.ldapconverter.ParseResult(attr);
+
+
+                    //    if(attr.Count > 1)
+                    //    {
+                    //        StringBuilder prop = new StringBuilder();
+                    //        foreach(var subAttr in attr)
+                    //        {
+
+                    //        }
+                    //    }
+                    //    try
+                    //    {
+                    //        attributes[entry.DistinguishedName].Add(Misc.Base64Decode(attr.ToString()).Result);
+                    //    }
+                    //    catch
+                    //    {
+                    //        attributes[entry.DistinguishedName].Add(attr.ToString());
+                    //    }
+                    //}
                 }
 
 
