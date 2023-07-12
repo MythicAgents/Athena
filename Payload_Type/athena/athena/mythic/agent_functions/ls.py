7a2604fdfe4ce4e87a93ad06cad941366c9329cf
checkymander <scottie.austin1991@gmail.com>
2023-07-12 08:28:35 -0400

    ripped apollo's ls/download parsing

--- Payload_Type/athena/athena/mythic/agent_functions/ls.py
+++ Payload_Type/athena/athena/mythic/agent_functions/ls.py
@@ -37,58 +37,66 @@ class DirectoryListArguments(TaskArguments):
         ]
 
 
-    #Path parsing originally by @djhohnstein https://github.com/MythicAgents/Apollo/blob/master/Payload_Type/apollo/mythic/agent_functions/ls.py
     async def strip_host_from_path(self, path):
         host = ""
-        if path[0] == "\\" and path[1] == "\\": #If the path starts with a UNC path, strip it out
-            final = path.find("\\", 2) #Find the next slash after the first two e.g. \\host\path it would find the third slash
+        if path[0] == "\\" and path[1] == "\\":
+            final = path.find("\\", 2)
             if final != -1:
-                host = path[2:final] #Set the host to the string between the first two slashes and the third slash
-                path = path[final+1:] #Set the path to the string after the third slash
-        return (host, path) #Return the host and path
+                host = path[2:final]
+                path = path[final+1:]
+        return (host, path)
 
-    #Argument parsing originally by @djhohnstein https://github.com/MythicAgents/Apollo/blob/master/Payload_Type/apollo/mythic/agent_functions/ls.py
-    #Potential inputs:
-        
-    #FileBrowser File
-    #{"host":"DESKTOP-GRJNOH2","path":"C:\\Users\\scott\\Downloads","full_path":"C:\\Users\\scott\\Downloads\\donut.tar.gz","file":"donut.tar.gz"}
-    
-    #FileBrowser Folder
-    #{"host":"DESKTOP-GRJNOH2","path":"C:\\Users\\scott\\Downloads","full_path":"C:\\Users\\scott\\Downloads\\donut","file":"donut"}
-    
-    #cmdline: C:\
-    #{"path": "C:", "host": ""}
-
-    #cmdline: C:\Users
-    #{"path": "C:\\Users", "host": ""}
-
-    #cmdline: localhost C:\users\scott
-    #{"host": "localhost", "path": "C:\\users\\scott"}
     async def parse_arguments(self):
-        print(self.command_line)
-        if len(self.command_line) == 0:
-            self.add_arg("host", "")
-            self.add_arg("path", ".")
-        else:
-            if self.command_line[0] == '{': #This is a file browser or modal input
+        if len(self.command_line) > 0:
+            # We'll never enter this control flow
+            if self.command_line[0] == '{':
                 temp_json = json.loads(self.command_line)
+                if "file" in temp_json.keys():
+                    # we came from the file browser
+                    host = ""
+                    path = temp_json['path']
+                    if 'file' in temp_json and temp_json['file'] != "":
+                        path += "\\" + temp_json['file']
+                    if 'host' in temp_json:
+                        # this means we have tasking from the file browser rather than the popup UI
+                        # the apfell agent doesn't currently have the ability to do _remote_ listings, so we ignore it
+                        host = temp_json['host']
 
-
-                if("full_path" in temp_json):
-                    self.add_arg("path", temp_json["full_path"])
+                    self.add_arg("host", host)
+                    self.add_arg("path", path)
+                    self.add_arg("file_browser", "true")
                 else:
-                    if(temp_json["path"] is None):
-                        self.add_arg("path", ".")
-                    else:
-                        self.add_arg("path", temp_json["path"])
-
-                if("host" in temp_json):
-                    self.add_arg("host", temp_json["host"])
-                else:
-                    self.add_arg("host", "")
+                    self.load_args_from_json_string(self.command_line)
+                    if self.get_arg("host") is not None and ":" in self.get_arg("host"):
+                        if self.get_arg("path") is None:
+                            self.add_arg("path", self.get_arg("host"))
+                        else:
+                            self.add_arg("path", self.get_arg("host") + " " + self.get_arg("path"))
+                        self.remove_arg("host")
+                    if self.get_arg("host") is not None and self.get_arg("path") is None:
+                        self.add_arg("path", self.get_arg("host"))
+                        self.set_arg("host", "")
             else:
-                if self.get_arg("path") == "":
-                    self.add_arg("path", ".")
+                args = await self.strip_host_from_path(self.command_line)
+                self.add_arg("host", args[0])
+                self.add_arg("path", args[1])
+                self.add_arg("file_browser", "true")
+        else:
+            self.add_arg("host", "")
+            self.add_arg("path", self.command_line)
+            self.add_arg("file_browser", "true")
+        if self.get_arg("path") is None:
+            self.add_arg("path", ".")
+        if self.get_arg("host") is None or self.get_arg("host") == "":
+            args = await self.strip_host_from_path(self.get_arg("path"))
+            self.add_arg("host", args[0])
+            self.add_arg("path", args[1])
+        elif self.get_arg("path")[:2] == "\\\\":
+            args = await self.strip_host_from_path(self.get_arg("path"))
+            self.add_arg("host", args[0])
+            self.add_arg("path", args[1])
+        if self.get_arg("path") is not None and self.get_arg("path")[-1] == "\\":
+            self.add_arg("path", self.get_arg("path")[:-1])
                 
 
 class DirectoryListCommand(CommandBase):
