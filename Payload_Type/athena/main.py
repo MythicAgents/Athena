502848f8af3d8f5a399e3f954613cf0c9d232b59
checkymander <scottie.austin1991@gmail.com>
2023-12-23 11:23:36 -0500

    added initial compilation and copying of plugins to bin directory

--- Payload_Type/athena/main.py
+++ Payload_Type/athena/main.py
@@ -2,6 +2,6 @@ import mythic_container
 from athena.mythic import *
 import subprocess
 
-#p = subprocess.Popen(["dotnet", "build", "--verbosity=q", "--nologo"], cwd="/Mythic/athena/agent_code/")
-
+#Kick off an initial load and obfuscation of the task plugins
+p = subprocess.Popen(["dotnet", "build","AgentPlugins.sln", "/p:PluginsOnly=True", "-c Release", "--verbosity=q", "--nologo"], cwd="/Mythic/athena/agent_code/")
 mythic_container.mythic_service.start_and_run_forever()
