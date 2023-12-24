5456c153e440d66834cc9c451f65116d05c673b6
checkymander <scottie.austin1991@gmail.com>
2023-12-23 20:38:10 -0500

    updating build_utils.py to wait for Agent.Models.dll before continuing

--- Payload_Type/athena/athena/agent_code/build_utils.py
+++ Payload_Type/athena/athena/agent_code/build_utils.py
@@ -1,16 +1,25 @@
 import sys
 import os
 import platform
+import subprocess
 import xml.etree.ElementTree as ET
+import time
 
-def create_obfuscar_xml(plugin_name, config, solution_dir):
-    assembly_search_path = os.path.join(solution_dir,"Agent.Models","bin",config,"net7.0")
-    in_path = get_interim_build_path(plugin_name, config, solution_dir)
-    out_path = get_obfuscated_build_path(plugin_name, config, solution_dir)
-    plugin_path = os.path.join(get_interim_build_path(plugin_name, config, solution_dir), plugin_name + ".dll")
+def create_obfuscar_xml(plugin_name, config, project_dir):
+    assembly_search_path = os.path.join(project_dir.replace(plugin_name,""),"Agent.Models", "bin",config,"net7.0")
 
-    obfuscar_xml_content = f'''
-<?xml version='1.0'?>
+    if(not os.path.exists(assembly_search_path)):
+        print("!!!!!!!!!!!!! Building Agent.Models.dll !!!!!!!!!!!!!")
+        try:
+            build_model_dll(plugin_name, project_dir, config)
+        except:
+            wait_for_file(os.path.join(project_dir.replace(plugin_name,""),"Agent.Models", "bin",config,"net7.0","Agent.Models.dll"))
+
+    in_path = get_interim_build_path(plugin_name, config, project_dir)
+    out_path = get_obfuscated_build_path(plugin_name, config, project_dir)
+    plugin_path = os.path.join(get_interim_build_path(plugin_name, config, project_dir), plugin_name + ".dll")
+
+    obfuscar_xml_content = f'''<?xml version='1.0'?>
 <Obfuscator>
 	<Var name="InPath" value="{in_path}" />
 	<Var name="OutPath" value="{out_path}" />
@@ -27,12 +36,14 @@ def create_obfuscar_xml(plugin_name, config, solution_dir):
 	<AssemblySearchPath path="{assembly_search_path}" />
 </Obfuscator>
     '''
-    obfuscar_path = get_obfuscar_xml_path(plugin_name, solution_dir)
+    obfuscar_path = get_obfuscar_xml_path(plugin_name, project_dir)
     # Write obfuscar.xml to the specified path if it doesn't exist
-    if not os.path.exists(obfuscar_path):
-        with open(obfuscar_path, 'w') as xml_file:
-            xml_file.write(obfuscar_xml_content)
-        print(f"Default obfuscar.xml created at {obfuscar_path}")
+    if os.path.exists(obfuscar_path):
+        os.remove(obfuscar_path)
+
+    with open(obfuscar_path, 'w') as xml_file:
+        xml_file.write(obfuscar_xml_content)
+    print(f"Default obfuscar.xml created at {obfuscar_path}")
 
 def run_obfuscator(obfuscar_exe_path, obfuscar_config_path):
     # Determine the platform (Windows or Linux)
@@ -43,22 +54,26 @@ def run_obfuscator(obfuscar_exe_path, obfuscar_config_path):
 
     # Execute the obfuscator command
     try:
-        #print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-        #print("Running Command: " + obfuscator_command)
-        #print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-        os.system(obfuscator_command)
-        print("Obfuscation completed successfully.")
+        command = [obfuscar_exe_path, os.path.join(obfuscar_config_path, "obfuscar.xml")]
+
+        # Start the process asynchronously
+        process = subprocess.Popen(command)
+
+        # Wait for the process to complete
+        process.wait()
+
+        process.communicate()
     except Exception as e:
         print(f"Error during obfuscation: {e}")
 
-def get_obfuscar_xml_path(plugin_name, solution_dir):
-    return os.path.join(solution_dir,"obfuscar.xml")
+def get_obfuscar_xml_path(plugin_name, project_dir):
+    return os.path.join(project_dir,"obfuscar.xml")
 
-def get_interim_build_path(plugin_name, config, solution_dir):
-    return os.path.join(solution_dir,plugin_name,"obj",config,"net7.0")
+def get_interim_build_path(plugin_name, config, project_dir):
+    return os.path.join(project_dir,"obj",config,"net7.0")
 
-def get_obfuscated_build_path(plugin_name, config, solution_dir):
-    return os.path.join(get_interim_build_path(plugin_name, config, solution_dir), "Obfuscated")
+def get_obfuscated_build_path(plugin_name, config, project_dir):
+    return os.path.join(get_interim_build_path(plugin_name, config, project_dir), "Obfuscated")
 
 def get_plugin_dir(plugin_name, solution_dir):
     return os.path.join(solution_dir,plugin_name)
@@ -70,6 +85,41 @@ def get_obfuscar_exe_path():
     else:
         return os.path.join(os.path.expanduser("~"),".dotnet", "tools", "obfuscar.console")
 
+def build_model_dll(plugin_name, project_dir, configuration):
+    models_proj_path = os.path.join(project_dir.replace(plugin_name,""),"Agent.Models", "Agent.Models.csproj")
+
+    # Construct the build command based on the platform
+    build_command = f'dotnet build {models_proj_path} -c {configuration}'
+
+    # Execute the build command
+    try:
+        command = ["dotnet", "build", models_proj_path, "-c", "Release"]
+
+        # Start the process asynchronously
+        process = subprocess.Popen(command)
+
+        # Wait for the process to complete
+        process.wait()
+
+        process.communicate()
+    except Exception as e:
+        print(f"Error during build: {e}")
+
+def wait_for_file(file_path, timeout_seconds=60):
+    start_time = time.time()
+
+    while not os.path.exists(file_path):
+        if time.time() - start_time > timeout_seconds:
+            print(f"Timeout waiting for {file_path} to exist.")
+            return False
+
+        # Adjust the sleep duration based on your requirements
+        time.sleep(1)
+
+    print(f"{file_path} found.")
+    return True
+
+
 def main():
     # Check if the correct number of command-line arguments is provided
     if len(sys.argv) != 4:
@@ -78,15 +128,15 @@ def main():
 
     # Get command-line arguments
     plugin_name = sys.argv[1].replace('\'','')
-    solution_dir = os.getcwd()
+    project_dir = os.getcwd()
     #solution_dir = sys.argv[2]
     configuration = sys.argv[3]
 
     # Create default obfuscar.xml
-    create_obfuscar_xml(plugin_name, configuration, solution_dir)
+    create_obfuscar_xml(plugin_name, configuration, project_dir)
 
     # Run obfuscator
-    run_obfuscator(get_obfuscar_exe_path(), solution_dir)
+    run_obfuscator(get_obfuscar_exe_path(), project_dir)
 
 if __name__ == "__main__":
     main()
