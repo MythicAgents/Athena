740330866d33321c35ae45f04e07a587ccd3a6db
checkymander <scottie.austin1991@gmail.com>
2024-01-07 23:15:32 -0500

    update jxa command to hopefully work

--- Payload_Type/athena/athena/agent_code/inject-shellcode-linux/inject-shellcode.cs
+++ Payload_Type/athena/athena/agent_code/inject-shellcode-linux/inject-shellcode.cs
@@ -1,26 +1,22 @@
 ﻿using Agent.Interfaces;
 using Agent.Models;
-using System.Runtime.InteropServices;
-using System.Text;
-using OSXIntegration.Framework;
-using Agent.Framework;
 
 namespace Agent
 {
     public class Plugin : IPlugin
     {
-        public string Name => "jxa";
+        public string Name => "inject-shellcode";
         private IMessageManager messageManager { get; set; }
+        private ITokenManager tokenManager { get; set; }
 
         public Plugin(IMessageManager messageManager, IAgentConfig config, ILogger logger, ITokenManager tokenManager)
         {
             this.messageManager = messageManager;
+            this.tokenManager = tokenManager;
         }
 
         public async Task Execute(ServerJob job)
         {
-            Dictionary<string, string> args = new Dictionary<string, string>();
-            await messageManager.WriteLine(AppleScript.Run(args["code"]), job.task.id, true);
         }
     }
 }
