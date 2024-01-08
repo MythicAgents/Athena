36beb10446946dde936ef371221c383ec417159c
checkymander <scottie.austin1991@gmail.com>
2024-01-08 16:06:30 -0500

    update to plist dump

--- Payload_Type/athena/athena/agent_code/mac_bundle.py
+++ Payload_Type/athena/athena/agent_code/mac_bundle.py
@@ -1,6 +1,6 @@
 import os
 import shutil
-from plistlib import writePlist
+from plistlib import dump
 import argparse
 
 def create_app_bundle(app_name, executable_path, output_dir="."):
@@ -25,7 +25,7 @@ def create_app_bundle(app_name, executable_path, output_dir="."):
 
     info_plist_path = os.path.join(contents_path, "Info.plist")
     with open(info_plist_path, "wb") as plist_file:
-        writePlist(info_plist_content, plist_file)
+        dump(info_plist_content, plist_file)
 
     return bundle_path
 
