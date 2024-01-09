160524a2edd52763a0629c8beeb1199861dd1621
checkymander <scottie.austin1991@gmail.com>
2024-01-09 17:58:36 -0500

    asdf

--- Payload_Type/athena/athena/agent_code/exec/exec.cs
+++ Payload_Type/athena/athena/agent_code/exec/exec.cs
@@ -47,14 +47,7 @@ namespace Agent
                 return;
             }
 
-            if(await this.spawner.Spawn(new SpawnOptions()
-            {
-                parent = args.parent,
-                commandline = args.commandline,
-                output = args.output,
-                task_id = job.task.id,
-                spoofedcommandline = args.spoofedcommandline
-            }))
+            if (await this.spawner.Spawn(args.getSpawnOptions(job.task.id)))
             {
                 await messageManager.AddResponse(new ResponseResult()
                 {
