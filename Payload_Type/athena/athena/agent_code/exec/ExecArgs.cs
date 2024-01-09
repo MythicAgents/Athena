d14de5da9fe003038bcc3e7fcb3674e5b3bc82c3
checkymander <scottie.austin1991@gmail.com>
2024-01-09 17:50:20 -0500

    make public

--- Payload_Type/athena/athena/agent_code/exec/ExecArgs.cs
+++ Payload_Type/athena/athena/agent_code/exec/ExecArgs.cs
@@ -8,23 +8,23 @@ using System.Threading.Tasks;
 
 namespace Agent
 {
-    internal class ExecArgs
+    public class ExecArgs
     {
         public int parent { get; set; }
         public string commandline { get; set; }
         public string spoofedcommandline { get; set; }
         public bool output { get; set; }
 
-        //public SpawnOptions getSpawnOptions(string task_id)
-        //{
-        //    return new SpawnOptions()
-        //    {
-        //        parent = this.parent,
-        //        commandline = this.commandline,
-        //        output = this.output,
-        //        task_id = task_id,
-        //        spoofedcommandline = this.spoofedcommandline
-        //    };
-        //}
+        public SpawnOptions getSpawnOptions(string task_id)
+        {
+            return new SpawnOptions()
+            {
+                parent = this.parent,
+                commandline = this.commandline,
+                output = this.output,
+                task_id = task_id,
+                spoofedcommandline = this.spoofedcommandline
+            };
+        }
     }
 }
