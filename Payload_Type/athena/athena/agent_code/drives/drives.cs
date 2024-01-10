53243349ab154c6123bc8ac9071647b6a10bc1ce
checkymander <scottie.austin1991@gmail.com>
2024-01-10 16:41:41 -0500

    update drives

--- Payload_Type/athena/athena/agent_code/drives/drives.cs
+++ Payload_Type/athena/athena/agent_code/drives/drives.cs
@@ -5,6 +5,13 @@ using System.Text;
 
 namespace Agent
 {
+    public class DriveObject
+    {
+        public string DriveName { get; set; }
+        public string DriveType { get; set; }
+        public long FreeSpace { get; set; }
+        public long TotalSpace { get; set; }
+    }
     public class Plugin : IPlugin
     {
         public string Name => "drives";
@@ -18,45 +25,26 @@ namespace Agent
 
         public async Task Execute(ServerJob job)
         {
+            Console.WriteLine("Getting Drives.");
             try
             {
-                List<dynamic> driveInfo = new List<dynamic>();
+                List<DriveObject> driveOutput = new List<DriveObject>();
                 var drives = DriveInfo.GetDrives();
-                StringBuilder sb = new StringBuilder();
-                foreach(var drive in drives)
+                foreach (var drive in drives)
                 {
-                    try
+                    driveOutput.Add(new DriveObject()
                     {
-                        dynamic dyn = new System.Dynamic.ExpandoObject();
-                        dyn.DriveName = drive.Name;
-                        dyn.DriveType = drive.DriveType;
-                        dyn.FreeSpace = drive.TotalFreeSpace / 1000000000;
-                        dyn.TotalSpace = drive.TotalSize / 1000000000;
-                        driveInfo.Add(dyn);
-                    }
-                    catch (Exception e)
-                    {
-                        await messageManager.AddResponse(new ResponseResult()
-                        {
-                            task_id = job.task.id,
-                            user_output = e.ToString(),
-                            completed = true
-                        });
-                    }
-                    //dyn.TotalFreeSpace = drive.TotalFreeSpace / 1000000000;
-                    //dyn.TotalSize = drive.TotalSize;
-                    //dyn.VolumeLabel = drive.VolumeLabel;
-                    //dyn.IsReady = drive.IsReady;
-                    //dyn.RootDirectory = drive.RootDirectory;
-                    //dyn.DriveFormat = drive.DriveFormat;
-                    //dyn.AvailableFreeSpace = drive.AvailableFreeSpace;
+                        DriveName = drive.Name,
+                        DriveType = drive.DriveType.ToString(),
+                        FreeSpace = drive.TotalFreeSpace / 1000000000,
+                        TotalSpace = drive.TotalSize / 1000000000
+                    });
                 }
 
-                string output = JsonSerializer.Serialize(driveInfo);
                 await messageManager.AddResponse(new ResponseResult()
                 {
                     task_id = job.task.id,
-                    user_output = output,
+                    user_output = JsonSerializer.Serialize(driveOutput),
                     completed = true
                 });
             }
