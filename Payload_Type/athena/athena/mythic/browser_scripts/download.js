b4725a04d52944c5f8dcd2de66b3efd3fc995df2
checkymander <scottie.austin1991@gmail.com>
2024-01-10 17:18:57 -0500

    pretty-fy json download

--- Payload_Type/athena/athena/mythic/browser_scripts/download.js
+++ Payload_Type/athena/athena/mythic/browser_scripts/download.js
@@ -7,18 +7,13 @@ function(task, responses){
     }else if(task.completed){
         if(responses.length > 0){
             try{
-                let data = JSON.parse(responses[0]);
+                let data = JSON.parse(responses[1]);
                 return {"download":[{
                         "agent_file_id": data["file_id"],
                         "variant": "contained",
                         "name": "Download",
                         "plaintext": "Download the file here: "
-                }], "search": [{
-                    "plaintext": "View on the search page here: ",
-                        "hoverText": "opens a new search page",
-                        "search": "tab=files&searchField=Filename&search=" + task.display_params,
-                        "name": "Click Me!"
-                    }]};
+                }]};
             }catch(error){
                 const combined = responses.reduce( (prev, cur) => {
                     return prev + cur;
@@ -33,7 +28,7 @@ function(task, responses){
     }else if(task.status === "processed"){
         if(responses.length > 0){
             const task_data = JSON.parse(responses[0]);
-            return {"plaintext": "Downloading a file with " + task_data["total_chunks"] + " total chunks..."};
+            return {"plaintext": "Downloading a file with " + task_data["totalChunks"] + " total chunks..."};
         }
         return {"plaintext": "No data yet..."}
     }else{
