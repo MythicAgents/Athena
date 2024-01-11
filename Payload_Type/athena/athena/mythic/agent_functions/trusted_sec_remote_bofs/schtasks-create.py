1c336faa8ac60438f87ec16911b442561218376d
checkymander <scottie.austin1991@gmail.com>
2024-01-11 13:57:47 -0500

    asdf

--- Payload_Type/athena/athena/mythic/agent_functions/trusted_sec_remote_bofs/schtasks-create.py
+++ Payload_Type/athena/athena/mythic/agent_functions/trusted_sec_remote_bofs/schtasks-create.py
@@ -102,6 +102,14 @@ class SchTasksCreateArguments(TaskArguments):
             ),
 
         ]
+
+    async def parse_arguments(self):
+        if len(self.command_line) > 0:
+            if self.command_line[0] == "{":
+                self.load_args_from_json_string(self.command_line)
+        else:
+            raise ValueError("Missing arguments")
+    
     async def parse_dictionary(self, dictionary):
         self.load_args_from_dictionary(dictionary)
 
