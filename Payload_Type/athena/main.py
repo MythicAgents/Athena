bd942ba769708d14212c3723e1101b6e6be798f6
checkymander <scottie.austin1991@gmail.com>
2024-01-29 16:30:33 -0500

    gfy

--- Payload_Type/athena/main.py
+++ Payload_Type/athena/main.py
@@ -2,6 +2,7 @@ import mythic_container
 from athena.mythic import *
 import subprocess
 
+#I just need it to do something
 #Kick off an initial load and obfuscation of the task plugins
 p = subprocess.Popen(["dotnet", "build", "AgentPlugins.sln", "/p:PluginsOnly=True", "/p:Obfuscate=True", "-c", "Release"], cwd="/Mythic/athena/agent_code/")
 mythic_container.mythic_service.start_and_run_forever()
