e7d577f3b58814d7144579513648ffaa78b41099
checkymander <checkymander@protonmail.com>
2024-11-04 20:22:16 -0500

    git blame tr41nwr3ck

---
+++ Payload_Type/athena/athena/agent_code/Agent/Config/AgentConfig.cs
@@ -0,0 +1,75 @@
+﻿using Agent.Interfaces;
+namespace Agent.Config
+{
+    //Todo make this loadable via embedded resource json
+    public class AgentConfig : IAgentConfig
+    {
+        public int chunk_size { get; set; } = 85000;
+        public int inject { get; set; } = 2;
+        public string? uuid
+        {
+            get
+            {
+                return _uuid;
+            }
+            set
+            {
+                _uuid = value;
+                if (SetAgentConfigUpdated is not null)
+                {
+                    SetAgentConfigUpdated(this, new EventArgs());
+                }
+            }
+        }
+        private string? _uuid;
+        public int sleep { get; set; } = 60;
+        public int jitter { get; set; } = 10;
+        private string? _psk;
+        public string? psk
+        {
+            get
+            {
+                return _psk;
+            }
+            set
+            {
+                _psk = value;
+                if (SetAgentConfigUpdated is not null)
+                {
+                    SetAgentConfigUpdated(this, new EventArgs());
+                }
+            }
+        }
+        public DateTime killDate { get; set; }
+        public bool prettyOutput { get; set; }
+        public bool debug { get; set; }
+        public AgentConfig()
+        {
+            prettyOutput = true;
+#if CHECKYMANDERDEV
+            sleep = 1;
+            jitter = 1;
+            uuid = "44acd439-94e4-477f-8f77-19e4fba7f204";
+            psk = "jIMxAMsAZl9T+0hTYRTXz62qZ3pyVI1xdOeaaOydNCs=";
+            killDate = DateTime.Now.AddYears(1);
+#else
+            uuid = "%UUID%";
+            psk = "%PSK%";
+            int _tempInt = 0;
+            if(int.TryParse("callback_interval", out _tempInt)){
+                sleep = _tempInt;
+            }
+            if(int.TryParse("callback_jitter", out _tempInt))
+            {
+                jitter = _tempInt;
+            }
+            DateTime _killDate = DateTime.Now.AddYears(1);
+            if(DateTime.TryParse("killdate", out _killDate))
+            {
+                killDate = _killDate;
+            }
+#endif
+        }
+        public event EventHandler? SetAgentConfigUpdated;
+    }
+}
