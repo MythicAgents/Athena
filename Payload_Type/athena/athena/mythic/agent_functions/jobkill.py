ccbcdd5240072a589cd2880a8bb0a8e80110648e
checkymander <checkymander@protonmail.com>
2024-12-05 23:43:27 -0500

    fix jobkill id parsing

--- Payload_Type/athena/athena/mythic/agent_functions/jobkill.py
+++ Payload_Type/athena/athena/mythic/agent_functions/jobkill.py
@@ -3,22 +3,23 @@ from mythic_container.MythicRPC import *
 
 
 class JobKillArguments(TaskArguments):
-
     def __init__(self, command_line, **kwargs):
         super().__init__(command_line, **kwargs)
-        self.args = [            
+        self.args = [
             CommandParameter(
                 name="id",
-                cli_name="id",
-                display_name="Id",
-                description="The task to kill",
                 type=ParameterType.String,
+                description="ID of the job to kill",
             )]
 
     async def parse_arguments(self):
-        if len(self.command_line.strip()) == 0:
-            raise Exception("You must specify a task id for use with jobkill.\n\tUsage: {}".format(JobKillCommand.help_cmd))
-        pass
+        if len(self.command_line) > 0:
+            if self.command_line[0] == "{":
+                self.load_args_from_json_string(self.command_line)
+            else:
+                self.add_arg("id", self.command_line)
+        else:
+            raise ValueError("Missing Arguments")
 
 
 class JobKillCommand(CommandBase):
